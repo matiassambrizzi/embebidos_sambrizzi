a2fe8155fe35d77d5bc0fcca7995351a1df025f4
matiassambrizzi <msambrizzi@fi.uba.ar>
2020-06-01 11:23:46 -0300

    Started my_gpio_init(), added some structs
    gpio tested on hardware

--- TP2/build/test/dependencies/test_my_gpio_runner.d
+++ TP2/build/test/dependencies/test_my_gpio_runner.d
@@ -4,5 +4,4 @@ build/test/out/c/test_my_gpio_runner.o: \
  /home/matias/.rbenv/versions/2.7.1/lib/ruby/gems/2.7.0/gems/ceedling-0.30.0/vendor/unity/src/unity_internals.h \
  /home/matias/.rbenv/versions/2.7.1/lib/ruby/gems/2.7.0/gems/ceedling-0.30.0/vendor/cmock/src/cmock.h \
  /home/matias/.rbenv/versions/2.7.1/lib/ruby/gems/2.7.0/gems/ceedling-0.30.0/vendor/cmock/src/cmock_internals.h \
- build/test/mocks/mock_scu_18xx_43xx.h build/test/mocks/scu_18xx_43xx.h \
- build/test/mocks/mock_gpio_18xx_43xx.h build/test/mocks/gpio_18xx_43xx.h
+ build/test/mocks/mock_cp_mcu_scu_gpio.h inc/mocks_inc/cp_mcu_scu_gpio.h
